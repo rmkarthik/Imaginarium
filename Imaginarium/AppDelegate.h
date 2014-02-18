6235f7df849fcb8e07084eef3647501801766e57
Karthikeyan Ramanathan <Karthik@Karthik-MacBook-Pro.local>
2014-02-18 19:25:53 +0530

    Initial Commit

---
+++ Imaginarium/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Imaginarium
+//
+//  Created by Karthikeyan Ramanathan on 18/02/14.
+//  Copyright (c) 2014 Karthik's App House. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Imaginarium/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Imaginarium
+//
+//  Created by Karthikeyan Ramanathan on 18/02/14.
+//  Copyright (c) 2014 Karthik's App House. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
